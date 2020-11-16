8e13f28ef46ca0c4b37c0603987261e5792f89cb
PatrickLan <patrick.lanisnik@gmail.com>
2020-11-16 12:12:51 +0100

    created allVehiclesDistanceOverview component

---
+++ src/components/allVehiclesDistanceOverview.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function AllVehiclesDistanceOverview() {
+  
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>AllVehiclesDistanceOverview</p>
+    </div>
+  );
+}
