5986e1c7dd348b834b325001f17bddac3a993a9b
PatrickLan <patrick.lanisnik@gmail.com>
2020-11-17 12:11:21 +0100

    added dashboard component

---
+++ components/dashboard.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+
+const Dashboard: React.FC = () => {  
+
+  return (
+      <div>
+         <p>Dashboard Component</p>
+      </div>
+  )
+}
+
+
+export default Dashboard;
