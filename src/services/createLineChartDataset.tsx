2a47d81ee7a3d83321cb7cf4f32a9629ca277753
PatrickLan <patrick.lanisnik@gmail.com>
2020-11-18 14:16:04 +0100

    added createDatasetFactoryFunction

---
+++ src/services/createLineChartDataset.tsx
@@ -0,0 +1,26 @@
+
+export function createDataset ( label: string, data: number[], rgbColor: string) {
+    return {
+        // label: "My Second dataset",
+        label: label,
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(184, 185, 210, .3)",
+        borderColor: rgbColor,
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: rgbColor,
+        pointBackgroundColor: rgbColor,
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: rgbColor,
+        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        // data: [28, 48, 40, 19, 86, 27, 90]
+        data: data
+    }
+}
