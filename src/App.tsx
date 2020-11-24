4a2bda02e94347fb56aded135259fb0b3f5813db
soelderich <philippohmes@gmx.de>
2020-11-24 09:36:35 +0100

    adding Spinner App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 import Dashboard from './components/dashboard/Dashboard';
+import { MDBContainer} from "mdbreact";
+import SpinnerPage from '../src/components/spinner/Spinner';
+
+let loaded = true;
 
 function App() {
-  //  const arr = await getHoursPerMonthAndCar();
-  // console.log("arr", arr)
 
-  // getUpdatedMonthArrayNumber();
- 
+  if (loaded)
+    return (
+      <div className="App">
+        <Dashboard />
+      </div>
+    );
   return (
-    <div className="App">
-      <Dashboard/>
-    </div>
-  );
+    <MDBContainer>
+        <div className="mx-auto">
+          <SpinnerPage></SpinnerPage>
+        </div>
+    </MDBContainer>
+  )
 }
-
+ 
 
 export default App;
---
+++ src/components/spinner/Spinner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const SpinnerPage = () => {
+  return (
+    <>
+      <div className="spinner-border spinner-border-xxl" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </>
+  );
+}
+
+export default SpinnerPage;
