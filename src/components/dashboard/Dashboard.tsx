671a0fa09cc947f3bb060f8957a4c61728166c7d
PatrickLan <patrick.lanisnik@gmail.com>
2020-12-10 14:54:23 +0100

    promise.all added

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -26,114 +26,117 @@ const Dashboard = ()  => {
   
       // Liste mit den Fahrzeugen fetchen
       carArray = await getCarData();
-  
-      //Alle Reservierungsdaten der letzten 12 Monate fetchen      
-      const reservationData: CarReservationResults[] = await getReservationsFromLastTwelveMonths();
 
-      //Array mit den letzten 12 Monaten importieren
-      let arrayWithLastTwelveMonths: string[] = await getUpdatedMonthArray();
+      getReservationsFromLastTwelveMonths();
+
   
-      //Arrays mit den Daten
-      let distanceArray: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-      let usedTimeArray: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      // //Alle Reservierungsdaten der letzten 12 Monate fetchen      
+      // const reservationData: CarReservationResults[] = await getReservationsFromLastTwelveMonths();
+      // console.log("TEST", reservationData)
+      // //Array mit den letzten 12 Monaten importieren
+      // let arrayWithLastTwelveMonths: string[] = await getUpdatedMonthArray();
+  
+      // //Arrays mit den Daten
+      // let distanceArray: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      // let usedTimeArray: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       
-      let carReservations:CarReservationResults[] = [];
+      // let carReservations:CarReservationResults[] = [];
   
-      // loop durch fahrzeugliste
-      for ( let i=0; i < carArray.length; i++) {
-        let actualCarId = carArray[i].id;
-        let label = carArray[i].type;
+      // // loop durch fahrzeugliste
+      // for ( let i=0; i < carArray.length; i++) {
+      //   let actualCarId = carArray[i].id;
+      //   let label = carArray[i].type;
        
-        //loop durch die reservierungen um die Reservierungen des gesuchten Fahrzeugs zu bekommen und im Array zu sichern
-        for ( let j=0; j < reservationData.length; j++) {
-          if (reservationData[j].vehicle.id === actualCarId){
-            carReservations.push(reservationData[j])
-          }
-        }
+      //   //loop durch die reservierungen um die Reservierungen des gesuchten Fahrzeugs zu bekommen und im Array zu sichern
+      //   for ( let j=0; j < reservationData.length; j++) {
+      //     if (reservationData[j].vehicle.id === actualCarId){
+      //       carReservations.push(reservationData[j])
+      //     }
+      //   }
   
-        //loop durch das Array mit den Reservierung des Fahrzeugs um die gefahrene Distanz zu bekommen
-        for (let i=0; i<carReservations.length; i++){
-          // zurückgelegt Distanz/Fahrzeug/Monat für die Distanzkomponente
-          let distancesTraveled: number | null = carReservations[i].distance;
+      //   //loop durch das Array mit den Reservierung des Fahrzeugs um die gefahrene Distanz zu bekommen
+      //   for (let i=0; i<carReservations.length; i++){
+      //     // zurückgelegt Distanz/Fahrzeug/Monat für die Distanzkomponente
+      //     let distancesTraveled: number | null = carReservations[i].distance;
   
-          // gefahrene Zeit/Fahrzeug/Monat für die Zeitkomponente
-          let bookedTime = (Date.parse(carReservations[i].reservation_end) - Date.parse(carReservations[i].reservation_start))/3600000;
+      //     // gefahrene Zeit/Fahrzeug/Monat für die Zeitkomponente
+      //     let bookedTime = (Date.parse(carReservations[i].reservation_end) - Date.parse(carReservations[i].reservation_start))/3600000;
   
-          //index des ReservierungsMonats in arrayWithLastTwelveMonths bekommen
-          let actualReservationMonth: string = carReservations[i].end_booked.slice(5, 7);
-          let searchedMonth: string = "";
+      //     //index des ReservierungsMonats in arrayWithLastTwelveMonths bekommen
+      //     let actualReservationMonth: string = carReservations[i].end_booked.slice(5, 7);
+      //     let searchedMonth: string = "";
           
-          switch(actualReservationMonth){
-            case "01":
-              searchedMonth = "Januar";
-              break;
-            case "02":
-              searchedMonth = "Februar";
-              break;
-            case "03":
-              searchedMonth = "März";
-              break;
-            case "04":
-              searchedMonth = "April";
-              break;
-            case "05":
-              searchedMonth = "Mai";
-              break;  
-            case "06":
-              searchedMonth = "Juni";
-              break;  
-            case "07":
-              searchedMonth = "Juli";
-              break;
-            case "08":
-              searchedMonth = "August";
-              break;
-            case "09":
-              searchedMonth = "September";
-              break;
-            case "10":
-              searchedMonth = "Oktober";
-              break;
-            case "11":
-              searchedMonth = "November";
-              break;
-            case "12":
-              searchedMonth = "Dezember";
-              break;
-         }
+      //   switch(actualReservationMonth){
+      //     case "01":
+      //       searchedMonth = "Januar";
+      //       break;
+      //     case "02":
+      //       searchedMonth = "Februar";
+      //       break;
+      //     case "03":
+      //       searchedMonth = "März";
+      //       break;
+      //     case "04":
+      //       searchedMonth = "April";
+      //       break;
+      //     case "05":
+      //       searchedMonth = "Mai";
+      //       break;  
+      //     case "06":
+      //       searchedMonth = "Juni";
+      //       break;  
+      //     case "07":
+      //       searchedMonth = "Juli";
+      //       break;
+      //     case "08":
+      //       searchedMonth = "August";
+      //       break;
+      //     case "09":
+      //       searchedMonth = "September";
+      //       break;
+      //     case "10":
+      //       searchedMonth = "Oktober";
+      //       break;
+      //     case "11":
+      //       searchedMonth = "November";              
+      //       break;
+      //     case "12":
+      //       searchedMonth = "Dezember";
+      //       break;
+      //   }
   
-         let searchedIndex: number =  arrayWithLastTwelveMonths.indexOf(searchedMonth);
+      //   let searchedIndex: number =  arrayWithLastTwelveMonths.indexOf(searchedMonth);
          
-         //Daten im Array der Komponenten in der selben Reihenfolge wie im Array mit Monaten sortieren       
-        if (distancesTraveled != null) {
-          distanceArray[searchedIndex] += Math.round(distancesTraveled);
-        } else {
-          distanceArray[searchedIndex] += 0;
-        }
-         usedTimeArray[searchedIndex] += Math.round(bookedTime);
-        }
+      //   //Daten im Array der Komponenten in der selben Reihenfolge wie im Array mit Monaten sortieren       
+      //   if (distancesTraveled != null) {
+      //     distanceArray[searchedIndex] += Math.round(distancesTraveled);
+      //   } else {
+      //     distanceArray[searchedIndex] += 0;
+      //   }
+        
+      //    usedTimeArray[searchedIndex] += Math.round(bookedTime);
+      //   }
+      
+      //   //Erstellen der Datasets zur grafischen Darstellung
+      //   let color: string = randomRGBColor();
+
+      //   let distanceDataset: Dataset  = createDataset(label, distanceArray, color);
+      //   distanceDatasets.push(distanceDataset);
+
+      //   let bookedTimeDataset: Dataset = createDataset(label, usedTimeArray, color);
+      //   bookedTimeDatasets.push(bookedTimeDataset);
+
+      //   //Reset nach dem Loop
+      //   usedTimeArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      //   distanceArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      //   carReservations = [];
+      // }
   
-    
-        //Erstellen der Datasets zur grafischen Darstellung
-        let color: string = randomRGBColor();
+      // let distanceDataline = createDataline(arrayWithLastTwelveMonths, distanceDatasets);
+      // setCarDistanceDataline(distanceDataline);
 
-        let distanceDataset: Dataset  = createDataset(label, distanceArray, color);
-        distanceDatasets.push(distanceDataset);
-
-        let bookedTimeDataset: Dataset = createDataset(label, usedTimeArray, color);
-        bookedTimeDatasets.push(bookedTimeDataset);
-
-        //Reset nach dem Loop
-        usedTimeArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        distanceArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        carReservations = [];
-      }
-  
-      let distanceDataline = createDataline(arrayWithLastTwelveMonths, distanceDatasets);
-      setCarDistanceDataline(distanceDataline);
-
-      let bookedTimeDataline = createDataline(arrayWithLastTwelveMonths, bookedTimeDatasets);
-      setCarBookedTimeDataline(bookedTimeDataline);
+      // let bookedTimeDataline = createDataline(arrayWithLastTwelveMonths, bookedTimeDatasets);
+      // setCarBookedTimeDataline(bookedTimeDataline);
     }
   
     useEffect (()=>{
--- src/services/reservations.tsx
+++ src/services/reservations.tsx
@@ -1,18 +1,40 @@
 import {CarReservationResults} from '../model/car_reservation_results_interface';
 import { getCarReservationData } from '../services/car_reservation_data';
 import moment from 'moment';
+import {getCountFetch} from '../services/reservationPages';
+import {getFetchData} from '../services/fetch_data_client';
 
 //Alle Reservierungsdaten der vorherigen letzten 12 Monate fetchen
-const getReservationsFromLastTwelveMonths = async (): Promise<CarReservationResults[]> => {
+// const getReservationsFromLastTwelveMonths = async (): Promise<CarReservationResults[]> => {
+    const getReservationsFromLastTwelveMonths = async () => {
 
     let first: string = "01";
     let firstDayOfThirteenMonthsAgo: string = moment().subtract(13, 'months').format('YYYY-MM-DD').substring(0, 8)+first;     
     let firstDayOfCurrentMonth: string = moment().format('YYYY-MM-DD').substring(0, 8)+first;
     let start_gte: string = `${firstDayOfThirteenMonthsAgo}T00:00`;
     let end_lte: string = `${firstDayOfCurrentMonth}T00:00`;
-    const reservationData: CarReservationResults[]  = await getCarReservationData(start_gte, end_lte);
 
-    return reservationData;
+    let firstUrl: string = `https://devteilautos.zemtu.com/api/v2/reservationaccounting/?reservation_start__gte=${start_gte}&reservation_end__lte=${end_lte}`
+    let pagesNumber = await getCountFetch(firstUrl);
+
+    let arrayWithUrls: string[] = [firstUrl];
+
+    // erzeugen eines Array mit den URLs aller Seiten der Reservierungen
+    for ( let i = 2; i <= pagesNumber; i++){        
+        arrayWithUrls.push(`https://devteilautos.zemtu.com/api/v2/?page=${i}&reservationaccounting/?reservation_start__gte=${start_gte}&reservation_end__lte=${end_lte}`)
+    }
+
+    let promises = arrayWithUrls.map(url => getFetchData(url));
+
+    Promise.all(promises).then(results => {
+        console.log("results", results)
+    })
+
+    // Promise.all(arrayWithUrls).then(values=> console.log("TEST", values))
+    // const reservationData: CarReservationResults[]  = await getCarReservationData(start_gte, end_lte);
+
+    // return reservationData;
+    return pagesNumber
 }
 
 export default getReservationsFromLastTwelveMonths;
