33c7ac4adfde2b91096db955476c071c586793b5
PatrickLan <patrick.lanisnik@gmail.com>
2020-12-11 10:42:35 +0100

    added missing typescript

--- src/services/fetch_data_client.tsx
+++ src/services/fetch_data_client.tsx
@@ -1,12 +1,14 @@
 import { CarPoolData } from '../model/car_pool_interface';
 import { CarReservations } from '../model/car-reservations_interface';
+import { CarPoolResults } from '../model/car_pool_results_interface';
+import {CarReservationResults} from '../model/car_reservation_results_interface';
 
 //Fetch all Data CarPoolData or CarReservations
 
-    export const getFetchData = async (url: string): Promise<CarPoolData[] | CarReservations[]> => {
+    export const getFetchData = async (url: string): Promise<CarReservations[] | CarPoolResults[] | CarReservationResults[]> => {
 
     const authorizationToken: string = "Token 2920a7e21d0b6ef854c0a53c7299403424086e11";
-    let fetchData: any;
+    let fetchData: CarReservations[] | CarPoolResults[] | CarReservationResults[] = [];
  
         await fetch(url, {
             method: "GET",
@@ -16,12 +18,11 @@ import { CarReservations } from '../model/car-reservations_interface';
             },
         })
             .then(response => response.json())
-
             .then(data => {
                 fetchData = data.results;
             })
             .catch(err => console.log('error', err))
- 
+
     return fetchData;
 };
 
